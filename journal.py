779c77007c03fee149a48fb21479867277176484
Charlie <store@Estebans-MacBook-Pro.local>
2014-10-13 12:50:07 -0700

    Add read functionality to journal. Add testing for that functionality.

--- journal.py
+++ journal.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from flask import Flask
 from flask import g
+from flask import render_template
 from contextlib import closing
 import os, psycopg2, datetime
 
@@ -18,6 +19,9 @@ DB_ENTRY_INSERT= """
 INSERT INTO entries (title, text, created) VALUES (%s, %s, %s)
 """
 
+DB_ENTRIES_LIST= """
+SELECT id, title, text, created FROM entries ORDER BY created DESC
+"""
 
 app= Flask(__name__)
 app.config['DATABASE']= os.environ.get(
@@ -63,9 +67,18 @@ def write_entry(title, text):
     now= datetime.datetime.utcnow()
     cur.execute(DB_ENTRY_INSERT, [title, text, now])
 
+def get_all_entries():
+    """return a list of all entreis as dicts"""
+    con= get_database_connection()
+    cur= con.cursor()
+    cur.execute(DB_ENTRIES_LIST)
+    keys= ('id', 'title', 'text', 'created')
+    return [dict(zip(keys, row)) for row in cur.fetchall()]
+
 @app.route('/')
-def hello():
-    return u'Hello World!'
+def show_entries():
+    entries= get_all_entries()
+    return render_template('list_entries.html', entries=entries)
 
 if __name__ == '__main__':
     app.run(debug=True)
